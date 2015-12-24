f5d3abd29c73ec0c1322bd4ed16c4a62c707a8f1
whayler1 <jstn@jstn.name>
2015-12-24 12:40:42 -0500

    grunt watch setup

--- gruntfile.js
+++ gruntfile.js
@@ -9,11 +9,22 @@ module.exports = function(grunt) {
           'assets/css/hens.css': 'src/less/hens.less'
         }
       }
+    },
+    watch: {
+      less: {
+        files: [
+          'src/less/hens.less'
+        ],
+        tasks: [
+          'less'
+        ]
+      }
     }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task(s).
   grunt.registerTask('default', ['less']);
